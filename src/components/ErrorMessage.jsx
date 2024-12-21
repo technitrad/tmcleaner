fb117261dc33c70e03053f3cebbbceaf967fd281
technitrad <30851839+technitrad@users.noreply.github.com>
2024-12-20 19:57:13 -0500

    Add files via upload

--- src/components/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorMessage({ message }) {
-  return (
-    <div className="bg-red-500 bg-opacity-10 border border-red-500 text-red-500 px-4 py-3 rounded-lg mb-6">
-      {message}
-    </div>
-  )
-}
